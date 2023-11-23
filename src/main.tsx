f1e1df1c692a66d87ae6901bfa8248e6d43996d4
Jeff <jeff2266@gmail.com>
2023-11-23 00:30:12 -0800

    Move image references

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import HamburgerMenu from './components/hamburgerMenu.tsx'
 import Carousel from './components/carousel.tsx'
-import twh1 from '../public/this-weeks-heartbreak-1.png'
-import twh2 from '../public/this-weeks-heartbreak-2.png'
-import partsStatus from '../public/parts-status-sort.gif'
-import rightClick from '../public/right-click-multiple.gif'
 
 ReactDOM.createRoot(document.getElementById('hamburger-menu')!).render(
   <React.StrictMode>
@@ -15,12 +11,12 @@ ReactDOM.createRoot(document.getElementById('hamburger-menu')!).render(
 
 ReactDOM.createRoot(document.getElementById('carousel-thisweeksheartbreak')!).render(
 	<React.StrictMode>
-		<Carousel images={[twh1, twh2]} />
+		<Carousel images={['this-weeks-heartbreak-1.png', 'this-weeks-heartbreak-2.png']} />
 	</React.StrictMode>
 )
 
 ReactDOM.createRoot(document.getElementById('carousel-sat')!).render(
 	<React.StrictMode>
-		<Carousel images={[partsStatus, rightClick]} />
+		<Carousel images={['parts-status-sort.gif', 'right-click-multiple.gif']} />
 	</React.StrictMode>
 )
